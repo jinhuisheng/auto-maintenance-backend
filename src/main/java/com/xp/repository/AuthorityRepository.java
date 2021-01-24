b7e6d9eaabf3877fd8109bea4379c21a68c12c03
huisheng.jin <huisheng.jinge@gmail.com>
2021-01-25 00:11:30 +0800

    Initial version of auto_maintenance generated by JHipster-6.10.5

---
+++ src/main/java/com/xp/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.xp.repository;
+
+import com.xp.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/xp/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package com.xp.service.mapper;
